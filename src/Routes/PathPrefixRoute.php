a339acdbf08eee0b6e6151387537602c8fba136c
Brooke Bryan <brooke@bajb.net>
2025-08-19 13:49:38 +0100

    Path prefix route

---
+++ src/Routes/PathPrefixRoute.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Packaged\Routing\Routes;
+
+use Packaged\Context\Context;
+use Packaged\Routing\Handler\Handler;
+use Packaged\Routing\Route;
+
+class PathPrefixRoute extends Route
+{
+  protected array $_paths = [];
+
+  /**
+   * @param string                  $pathPrefix
+   * @param Handler|string|callable $handler
+   *
+   * @return $this
+   */
+  public function addPath(string $pathPrefix, Handler|string|callable $handler): static
+  {
+    $this->_paths[$pathPrefix] = $handler;
+    return $this;
+  }
+
+  public function match(Context $context): bool
+  {
+    $path = $context->request()->path();
+    foreach($this->_paths as $pathPrefix => $handler)
+    {
+      if(str_starts_with($path, $pathPrefix))
+      {
+        $this->setHandler($handler);
+        return true;
+      }
+    }
+    return false;
+  }
+}
